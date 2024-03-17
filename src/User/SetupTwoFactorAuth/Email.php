4b5a46d1c61141620f36f5086b06fad7f3ada1bc
RedstonePfalz <55254344+RedstonePfalz@users.noreply.github.com>
2024-03-17 18:33:11 +0100

    feat(getuser-class): Add email OTP class

---
+++ src/User/SetupTwoFactorAuth/Email.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace ZitadelPhpClient\User\SetupTwoFactorAuth;
+
+use Exception;
+
+class Email
+{
+    private array $settings;
+    private int $userid;
+
+    /**Initialize the Email OTP setup
+     * @param $settings array The settings array
+     */
+    public function __construct(array $settings) {
+        $this->settings = $settings;
+    }
+
+    /**Set the userid
+     * @param $userid int User id
+     * @return void
+     */
+    public function setUserId(int $userid) {
+        $this->userid = $userid;
+    }
+
+    /**Add the OTP Email method
+     * @return void
+     * @throws Exception Returns an exception with an error code and a message if the communication with Zitadel fails
+     */
+    public function add() {
+        $this->request("POST");
+    }
+
+    /**Remove the OTP Email method from the user
+     * @return void
+     * @throws Exception Returns an exception with an error code and a message if the communication with Zitadel fails
+     */
+    public function remove() {
+        $this->request("DELETE");
+    }
+
+    /**
+     * @param $type
+     * @return void
+     * @throws Exception Returns an exception with an error code and a message if the communication with Zitadel fails
+     */
+    private function request($type) {
+        $token = $this->settings["userToken"];
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->settings["domain"] . "/v2beta/users/$this->userid/otp_email",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>"{}",
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/json",
+                "Accept: application/json",
+                "Authorization: Bearer $token"
+            ),
+        ));
+
+        $response = json_decode(curl_exec($curl));
+        curl_close($curl);
+        if(isset($response->code)) {
+            throw new Exception("Error-Code: " . $response->code . " Message: " . $response->message);
+        }
+    }
+}
